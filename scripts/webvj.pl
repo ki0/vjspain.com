7a9a293698afb1e2e4514e8ccdf860df25304d6e
Fran Rodríguez <fran.clasico@gmail.com>
2012-09-19 23:31:01 +0200

    New features on import script.

--- scripts/webvj.pl
+++ scripts/webvj.pl
@@ -6,16 +6,28 @@ use DBI;
 my $host = "localhost";
 my $db1 = "webvj";
 my $db2 = "vjspain";
+my $db3 = "foro";
 my $user = "root";
-my $pw = "bellota";
+my $pw = "";
 
-my $dbh = DBI->connect("dbi:mysql:database=$db1;host=$host", $user, $pw, {RaiseError => 1});
+my $dbh1 = DBI->connect("dbi:mysql:database=$db1;host=$host", $user, $pw, {RaiseError => 1});
+my $dbh2 = DBI->connect("dbi:mysql:database=$db2;host=$host", $user, $pw, {RaiseError => 1});
+my $dbh3 = DBI->connect("dbi:mysql:database=$db3;host=$host", $user, $pw, {RaiseError => 1});
 
 my $sql = "SELECT * FROM userlist";
-my $sth = $dbh->prepare($sql);
+my $sth = $dbh1->prepare($sql);
 $sth->execute();
-my $row = $sth->fetchrow_hashref();
+while ( my @row = $sth->fetchrow_array ){
+  print "@row\n";
+}
 
-print $row;
+$sql = "DELETE FROM comunidad WHERE visitas = 0 AND newsletter = 0";
+$sth = $dbh1->prepare($sql);
+$sth->execute();
 
-
+$sql = "SELECT fechaAlta, nombre, apellidos, web, email, usuario, salasana  FROM comunidad WHERE 1";
+$sth = $dbh1->prepare($sql);
+$sth->execute();
+while ( my @row = $sth->fetchrow_array ){
+  print "@row\n";
+}
