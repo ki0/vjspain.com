4c5045fa40912a11e4adfa8352ffb231f213f630
Fran Rodríguez <fran.clasico@gmail.com>
2012-10-05 01:56:24 +0200

    Insert categorias to wordpress bd

--- scripts/webvj.pl
+++ scripts/webvj.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+use 5.012;
 use warnings;
 use strict;
 use DBI;
@@ -26,7 +27,7 @@ while ( my $row = $sel->fetchrow_hashref ){
   my $ins = $dbh2->prepare("INSERT INTO 
                             wp_users (ID, user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name, spam, deleted) 
                             values (?,?,?,?,?,?,?,?,?,?,?,?)");
-  $ins->execute(3, $$row{'usuario'}, md5_hex($$row{'clave'}), $$row{'usuario'}, $$row{'email'}, "http://vjspain.com", $date, " ", 0, $$row{'usuario'}, 0, 0 );
+#  $ins->execute(3, $$row{'usuario'}, md5_hex($$row{'clave'}), $$row{'usuario'}, $$row{'email'}, "http://vjspain.com", $date, " ", 0, $$row{'usuario'}, 0, 0 );
 }
 
 my $del = $dbh1->prepare("DELETE FROM comunidad WHERE id = 30");
@@ -42,8 +43,37 @@ while ( my $row = $sel->fetchrow_hashref ){
   my $ins = $dbh2->prepare("INSERT INTO 
                             wp_users (ID, user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name, spam, deleted) 
                             values (?,?,?,?,?,?,?,?,?,?,?,?)");
-  $ins->execute($$row{'id'}, $$row{'usuario'}, md5_hex($$row{'salasana'}), $$row{'nombre'} . " " . $$row{'apellidos'}, $$row{'email'}, $$row{'web'}, $$row{'fechaAlta'}, " ", 0, $$row{'usuario'}, 0, 0 );
+#  $ins->execute($$row{'id'}, $$row{'usuario'}, md5_hex($$row{'salasana'}), $$row{'nombre'} . " " . $$row{'apellidos'}, $$row{'email'}, $$row{'web'}, $$row{'fechaAlta'}, " ", 0, $$row{'usuario'}, 0, 0 );
 }
+
+$del = $dbh2->prepare("DELETE FROM wp_terms WHERE 1");
+$del->execute();
+
+$sel = $dbh1->prepare("SELECT * FROM noticias_categorias WHERE 1");
+$sel->execute();
+while ( my $row = $sel->fetchrow_hashref ){
+  print "$$row{'id'}";
+  (my $term = $$row{'categoria'}) =~ s/_ (\w+)/$1/;
+  $term =~ s/(\w+)/\L$1/;
+  $term =~ s/(\w+) (\w+)/\L$1-$2/;
+  $term =~ s/(\w+) - (\w+)/\L$1-$2/;
+  $term =~ s/(\w+), (\w+)-y (\w+)/\L$1-$2-$3/;
+  print " $term $$row{'categoria'}\n";
+  my $ins = $dbh2->prepare("INSERT INTO 
+                            wp_terms (term_id, name, slug, term_group) 
+                            values (?,?,?,?)");
+                          #$ins->execute($$row{'id'}, $$row{'categoria'}, $term, 0 );
+}
+
+
+#$sel = $dbh1->prepare("SELECT * FROM noticias WHERE 1");
+#$sel->execute();
+#while ( my $row = $sel->fetchrow_hashref ){
+#  my $ins = $dbh2->prepare("INSERT INTO 
+#                            wp_users (ID, user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name, spam, deleted) 
+#                            values (?,?,?,?,?,?,?,?,?,?,?,?)");
+#                          #$ins->execute($$row{'id'}, $$row{'usuario'}, md5_hex($$row{'salasana'}), $$row{'nombre'} . " " . $$row{'apellidos'}, $$row{'email'}, $$row{'web'}, $$row{'fechaAlta'}, " ", 0, $$row{'usuario'}, 0, 0 );
+#}
 $dbh1->disconnect();
 $dbh2->disconnect();
 $dbh3->disconnect();
